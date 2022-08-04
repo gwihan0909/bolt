18814c92da80e22f4208215fdc012991d478c7f1
gwihan0909 <zarasolomon0909@gmail.com>
2022-08-04 11:06:42 +0100

    updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
  const app = require('./app')
 require('dotenv').config()
-const port = process.env.PORT || PORT
+const port = process.env.PORT || 8000
 
-app.listen(port, () => console.log("Server running @" +  port))
+app.listen(port, () => console.log("Server running @" +  port)) 
